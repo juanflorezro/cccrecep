582caf31aa64fc6e075eea9ac17f0a6ae59b0a74
juanflorezro <142103189+juanflorezro@users.noreply.github.com>
2023-09-29 16:00:40 -0500

    Update personas.jsx

--- src/home/main/registros/pages/personas.jsx
+++ src/home/main/registros/pages/personas.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import AOS from 'aos'
 
 export default function Personas(){
   const [datosUpdateUsuario, setDatosUpdateUsuario] = useState({
@@ -22,6 +22,7 @@ export default function Personas(){
   const [editar, setEditar] = useState(false)
   useEffect(()=>{
     cargarPersonas()
+    AOS.init()
   },[])
   const tokenN = (name) => {
     const cookies = document.cookie.split('; ')
@@ -51,7 +52,7 @@ export default function Personas(){
   const cargarPersonas = () => {
     setLoading(true)
     const token = tokenN('token')
-    axios.post('https://express.juanflow04flore.repl.co/usuarios/listaVisitante',{},{
+    axios.post('https://cccrecepcionbackend-n4au-dev.fl0.io/usuarios/listaVisitante',{},{
       headers: {
         authorization: token
       },
@@ -61,7 +62,6 @@ export default function Personas(){
       }
     })
     .then(doc => {
-      console.log(doc.data)
       const per = doc.data
       setPersonas(per)
       setLoading(false)
@@ -69,67 +69,87 @@ export default function Personas(){
     })
     .catch(err => {
       console.log(err)
+      setLoading(false)
+      Swal.fire({ icon: 'error', title: 'Oops...', text: '¡Error, Intente de nuevo o comuniquese con el Departamento Tic!' })
       setProgress(0)
     })
   }
   const handleEditarPersona = (e) => {
     e.preventDefault()
-    
+
     const token = tokenN('token')
-    console.log(token)
-    console.log(usuarioId)
-    console.log(datosUpdateVisitante)
-    console.log(datosUpdateUsuario)
-    setLoading(true)
-    var resultado = window.confirm('¿Quieres editar esta Persona? ->> '+datosUpdateUsuario.nombre)
-    if (resultado === true) {
-      axios.post('https://express.juanflow04flore.repl.co/usuarios/editarVisitante',{usuarioId,datosUpdateUsuario, datosUpdateVisitante},{
+    Swal.fire({
+      title: '¿Quieres Editar este Usuario?',
+      text: "Si ya valido que la informacion es correcta Oprimir 'Editar'",
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Editar'
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        axios.post('https://cccrecepcionbackend-n4au-dev.fl0.io/usuarios/editarVisitante',{usuarioId,datosUpdateUsuario, datosUpdateVisitante},{
         headers: {
         authorization: token
         }
       })
-      .then(doc => {
-        alert('Persona Editada Correctamente \n <<<<<<<  ' + '>>>>>>>>>')
-        console.log(doc.data)
+        .then(doc => {
         setPersonas([])
         cargarPersonas()
         setEditar(false)
-      })
-      .catch(err => {
+        setLoading(false)
+        Swal.fire(
+          '¡Guardado!',
+          'Acción realizada Correctamenete',
+          'success'
+        )  
+        })
+        .catch(err => {
         console.log(err)
-      })
-      } 
-    else { 
-        window.alert('Accion Cancelada')
-    }
-    
+        setEditar(false)
+        setLoading(false)
+        Swal.fire({ icon: 'error', title: 'Oops...', text: '¡Error, Intente de nuevo o comuniquese con el Departamento Tic!' })
+        })   
+      }
+    })
  }
-  const handleEliminarPersona = (persona) => {
-    const usuarioIdc = {
-      cedula: persona.usuario.cedula
-    }
+  const handleEliminarPersona = (usuario) => {
+    
     const token = tokenN('token')
-    console.log(usuarioIdc)
-    var resultado = window.confirm('¿Quieres Elimanar esta Persona? ->> ' + persona.usuario.cedula +'\n🚧🚧🚧 NOTA IMPORTANTE: 🚧🚧🚧 \n Si eliminas este Usuario se eliminaran todas sus Entradas y Salidas realizadas. \nSugerencia ❓: Descargue el historial antes de eliminar')
-    if (resultado === true) {
-      axios.post('https://express.juanflow04flore.repl.co/usuarios/eliminarUsuario',{usuarioIdc},{
+     Swal.fire({
+      title: '¿Quieres Elimanar este Usuario?',
+      text: "🚧🚧🚧 NOTA IMPORTANTE: 🚧🚧🚧 \nSi eliminas este Usuario se eliminaran todas sus Entradas y Salidas realizadas. \nSugerencia ❓: Descargue el historial antes de eliminar'",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Eliminar'
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        setLoading(true)
+        axios.post('https://cccrecepcionbackend-n4au-dev.fl0.io/usuarios/eliminarUsuario',{usuario},{
         headers: {
         authorization: token
         }
       })
-      .then(doc => {
-        alert('Persona Eliminada Correctamente \n <<<<<<<  ' + '>>>>>>>>>')
-        console.log(doc.data)
-        setPersonas([])
-        cargarPersonas()
-      })
-      .catch(err => {
+        .then(doc => {
+          setPersonas([])
+          cargarPersonas()
+          Swal.fire(
+              '¡Eliminado!',
+              'Acción realizada Correctamenete',
+              'success'
+            )
+          })
+        .catch(err => {
         console.log(err)
+        setLoading(false)
+        Swal.fire({ icon: 'error', title: 'Oops...', text: '¡Error, Intente de nuevo o comuniquese con el Departamento Tic!' })
       })
-      } 
-    else { 
-        window.alert('Accion Cancelada')
-    }
+      }
+    })
   }
   const editarPersona = (persona) => {
       setEditar(true)
@@ -155,13 +175,12 @@ export default function Personas(){
     console.log(buscare)
     if(buscare){
       const usuarioIdo = {
-        cedula: buscare,
-        recepcionista: tokenN('cedula')
+        cedula: buscare
       }
       setPersonas([])
       setLoading(true)
       const token = tokenN('token')
-      axios.post('https://express.juanflow04flore.repl.co/usuarios/listaVisitanteid',{usuarioIdo},{
+      axios.post('https://cccrecepcionbackend-n4au-dev.fl0.io/usuarios/listaVisitanteid',{usuarioIdo},{
         headers: {
           authorization: token
         }
@@ -175,17 +194,38 @@ export default function Personas(){
       })
       .catch(err => {
         console.log(err)
+        setLoading(false)
+        Swal.fire({ icon: 'error', title: 'Oops...', text: '¡Error, Intente de nuevo o comuniquese con el Departamento Tic!' })
       })
       
     }else{
       cargarPersonas()
     }
   }
+  const handleBuscarn = (nombre) => {
+    setPersonas([])
+    const token = tokenN('token')
+    axios.post('https://cccrecepcionbackend-n4au-dev.fl0.io/usuarios/listaVisitanten',{nombre},{
+      headers: {
+        authorization: token
+      }
+    })
+      .then(doc => {
+        const per = doc.data
+        setPersonas(per)
+        
+      })
+      .catch(err => {
+        console.log(err)
+        Swal.fire({ icon: 'error', title: 'Oops...', text: '¡Error, Intente de nuevo o comuniquese con el Departamento Tic!' })
+      })
+  }
   return(
     <>
       <div className = 'busqueda'>
         <div className = 'ccheckboxs'>
-          <div className='checkboxs'><input style={{padding: '7px', borderRadius: '50px', textAlign: 'center' }} type='number' placeholder='Buscar por # Cedula' onChange={(e) => handleBuscar(e.target.value)}/></div>
+          <div className='checkboxs'><input style={{padding: '7px', borderRadius: '50px', textAlign: 'center', border: 'none' }} type='number' placeholder='Buscar por # Cedula' onChange={(e) => handleBuscar(e.target.value)}/></div>
+          <div className='checkboxs'><input style={{padding: '7px', borderRadius: '50px', textAlign: 'center', border: 'none' }} type='text' placeholder='Buscar por Nombre' onChange={(e) => handleBuscarn(e.target.value)}/></div>
           <div className='checkboxs'>
             <button onClick={()=>{cargarPersonas()}} style={{ background: 'none', color: 'black', border: 'none', borderRadius: '50px', padding: '7px 20px',marginLeft: '5px' }}>
                ䷀
@@ -194,18 +234,19 @@ export default function Personas(){
         </div>
       </div> 
       {loading && <div>
-        <div class="container">
-          <div class="cargando">
-            <div class="pelotas"></div>
-            <div class="pelotas"></div>
-            <div class="pelotas"></div>
-            <span class="texto-cargando">Cargando...{progress ? <>
+        <div className="container">
+          <div className="cargando">
+            <div className="pelotas"></div>
+            <div className="pelotas"></div>
+            <div className="pelotas"></div>
+            <span className="texto-cargando">Cargando...{progress ? <>
               ✔
             </>: <></>}</span>
           </div>
         </div>  
       </div>}
-      {editar && <div>
+      {!loading && <>
+        {editar && <div>
         <>
                   <form onSubmit={handleEditarPersona} style={{display: loading ? 'none' : 'block', maxWidth: '720px', margin: '0 auto', backgroundColor: '#f5f5f5', padding: '20px', borderRadius: '4px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.5)', maxHeight: 'calc(100vh - 40px)', overflow: 'auto', marginTop: '20px'}}>
                     <h2 style={{ marginBottom: '20px', color: '#333', width: '100%', textAlign: 'center' }}
@@ -227,7 +268,7 @@ export default function Personas(){
                         value={datosUpdateUsuario.nombre}
                         onChange={handleInputChange}
                         required
-                        style={{ width: '34%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
+                        style={{ width: '31%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
                       />
                       <label style={{ color: '#555' }}> Apellido: </label>
                       <input
@@ -238,10 +279,31 @@ export default function Personas(){
                         value={datosUpdateUsuario.apellido}
                         onChange={handleInputChange}
                         required
-                        style={{ width: '34%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
+                        style={{ width: '31%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
                       />
                     </div>
-                    
+                    <div className="tel">
+                      <label style={{ color: '#555' }}>Telefono: </label>
+                      <input
+                        placeholder='exam: 3003331333'
+                        type="number"
+                        name="telefono"
+                        value={datosUpdateUsuario.telefono}
+                        onChange={handleInputChange}
+                        required
+                        style={{ width: '17%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
+                      />
+                      <label style={{ color: '#555'}}> Correo: </label>
+                      <input
+                        placeholder='exam: ejemplo@gmail.com'
+                        type="text"
+                        name="correo"
+                        value={datosUpdateUsuario.correo}
+                        onChange={handleInputChange}
+                        required
+                        style={{ width: '46.5%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
+                      />
+                    </div>
                     <div className="em">
                       <label style={{ color: '#555' }}>Empresa: </label>
                       <input
@@ -251,7 +313,7 @@ export default function Personas(){
                         value={datosUpdateVisitante.empresa}
                         onChange={handleInputEmpresa}
                         required
-                        style={{ width: '37%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
+                        style={{ width: '35%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
                       />
                       <label style={{ color: '#555' }}> Edad: </label>
                       <input
@@ -263,42 +325,8 @@ export default function Personas(){
                         required
                         style={{ width: '8%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
                       />
-                      <label style={{ color: '#555' }}> Tipo: </label>
-                      <select
-                        name="tipo"
-                        value={datosUpdateVisitante.tipo}
-                        onChange={handleInputTipo}
-                        required
-                        style={{ width: '18.8%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
-                      >
-                        <option value="">Seleccionar</option>
-                        <option value="Proyectos">Proyectos</option>
-                        <option value="Contratistas">Contratistas</option>
-                        <option value="Visitas">Visitas</option>
-                      </select>
                     </div>
-                    <div className="tel">
-                      <label style={{ color: '#555' }}>Telefono: </label>
-                      <input
-                        placeholder='exam: 3003331333'
-                        type="number"
-                        name="telefono"
-                        value={datosUpdateUsuario.telefono}
-                        onChange={handleInputChange}
-                        required
-                        style={{ width: '20%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
-                      />
-                      <label style={{ color: '#555'}}> Correo: </label>
-                      <input
-                        placeholder='exam: ejemplo@gmail.com'
-                        type="text"
-                        name="correo"
-                        value={datosUpdateUsuario.correo}
-                        onChange={handleInputChange}
-                        required
-                        style={{ width: '49.5%', padding: '8px', marginBottom: '10px', borderRadius: '4px', border: '1px solid #ccc' }}
-                      />
-                    </div>
+                    
                     <br></br>        
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
                       <button type="submit" style={{ display: loading ? 'none' : 'block', marginRight: '10px', padding: '8px 24.5px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>Editar</button>
@@ -309,14 +337,14 @@ export default function Personas(){
                 </>
                 
       </div>}
-      {!editar && <div  className='contenerdorobjetos'>
-        {personas.map((persona, index) => (
-          <div className='tarejta'>
+        {!editar && <div  className='contenerdorobjetos' style={{ overflowX: 'auto', maxHeight: '400px', padding: '20px'}} data-aos="zoom-in">
+        {personas.map((persona) => (
+          <div className='tarejta'  key = {persona._id}>
             <label className='t_nombre'>{persona.usuario.nombre} {persona.usuario.apellido}</label>
             <label title='Cedula'  className='t_cc'>{persona.usuario.cedula}</label>
-            <label title='Tipo' className='t_tipo'>{persona.tipo}</label>
+            <label title='Tipo' className='t_tipo'>{persona.usuario.correo }</label>
             <div className='t_botones'>
-              <button title={'Detalles de '+persona.usuario.nombre} onClick={() => alert('\n<<<<<<<<<<<<< Mas Detalles Personales>>>>>>>>>>>>>>\n\n'+'Nombre: '+persona.usuario.nombre+'\nApellidos: '+persona.usuario.apellido+'\nCorreo: '+persona.usuario.correo+'\nTelefono: '+persona.usuario.telefono+'\nEdad: '+persona.usuario.edad+'\nCedula: '+persona.usuario.cedula+'\nTipo: '+persona.tipo+'\nEmpresa: '+persona.empresa)} >📄</button>
+              <button title={'Detalles de '+persona.usuario.nombre} onClick={() => Swal.fire('Mas Detalles Personales\n\n'+'Nombre: '+persona.usuario.nombre+'\nApellidos: '+persona.usuario.apellido+'\nCorreo: '+persona.usuario.correo+'\nTelefono: '+persona.usuario.telefono+'\nEdad: '+persona.usuario.edad+'\nN° Documento: '+persona.usuario.cedula+'\nEmpresa: '+persona.empresa)} >📄</button>
               <button title={'Editar a '+persona.usuario.nombre} onClick ={()=>{editarPersona(persona)}}>✏️</button>
               <button title={'Eliminar a '+persona.usuario.nombre} onClick ={()=>{handleEliminarPersona(persona)}}>✂</button>
             </div>
@@ -324,7 +352,7 @@ export default function Personas(){
           </div>
         ))}
       </div>}
-      
+      </>}
     </>
   )
 }
