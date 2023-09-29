26ca386ac82127cef8da4d6ea9e59bcacb492f36
juanflorezro <142103189+juanflorezro@users.noreply.github.com>
2023-09-29 15:54:27 -0500

    Update navegacion.jsx

--- src/home/main/registros/navegacion.jsx
+++ src/home/main/registros/navegacion.jsx
@@ -3,6 +3,7 @@ import Home from '../../Home'
 import EntradasSalidas from './pages/entradasSalidas'
 import Activos from './pages/activos'
 import Personas from './pages/personas'
+import Arrys from './pages/arrys'
 const Tab = ({ tab, activeTab, setActiveTab }) => {
   return (
     <div
@@ -19,6 +20,19 @@ export default function Tabs () {
 
   return (
     <>
+      <div class="contenedor-card-item">
+		      <div class="contenedor-card-item-wrapper">
+		        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9pwuN55xOQq48Ebu_rZKV287ak67N8yjTDQ&usqp=CAU" alt=""/>
+		        <div class="contenedor-info">
+		          <div class="info">
+		            <p class="titulo"></p>
+		            <span class="categoria">Ayuda</span>
+		          </div>
+		          <div class="fondo"></div>
+		        </div>
+		      </div>
+		    </div>
+      <div className = 'mas_vida'></div>
       <div style={{  display: "flex", height: '100vh', width: '100%'}}>
         <Home/>
         <div style={{  display: "flex", flexDirection: 'column' , width: '100%'}}>
@@ -27,12 +41,14 @@ export default function Tabs () {
             <div className="tab-container">
               <Tab tab="Entradas y Salidas" activeTab={activeTab} setActiveTab={setActiveTab} />
               <Tab tab="Activos" activeTab={activeTab} setActiveTab={setActiveTab} />
-              <Tab tab="Personas" activeTab={activeTab} setActiveTab={setActiveTab} />
+              <Tab tab="Usuarios" activeTab={activeTab} setActiveTab={setActiveTab} />
+              <Tab tab="Arrys" activeTab={activeTab} setActiveTab={setActiveTab} />
             </div>
             <div className="tab-content">
               {activeTab === 'Entradas y Salidas' && <div><EntradasSalidas/></div>}
               {activeTab === 'Activos' && <div><Activos/></div>}
-              {activeTab === 'Personas' && <div><Personas/></div>}
+              {activeTab === 'Usuarios' && <div><Personas/></div>}
+              {activeTab === 'Arrys' && <div><Arrys/></div>}
             </div>
           </div>
         </div>
