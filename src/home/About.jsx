a0164da33d655df7068e3719b93a96b36ec36f68
juanflorezro <142103189+juanflorezro@users.noreply.github.com>
2023-10-10 08:52:25 -0500

    Update About.jsx

--- src/home/About.jsx
+++ src/home/About.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import AOS from 'aos'
+import Git from '../../public/git.png'
 export default function About(){
   useEffect(()=>{
     AOS.init()
@@ -17,9 +18,9 @@ export default function About(){
           <div className= 'has'>
          <div className= 'has-2'>
            
-           <div className='git'> <a href = 'https://github.com/juanflorezro/cccrecepcion'><img className= 'imagen_fondo' src = '/git.png'/> </a></div>
+           <div className='git'> <a href = 'https://github.com/juanflorezro/cccrecepcion'><img className= 'imagen_fondo' src = '../../public/git.png'/> </a></div>
            <div className= 'imagen_fondo2'> 
-            <a aria-label="Chat on WhatsApp" href="https://wa.me/573003331333"><img alt="Chat on WhatsApp" src="./what.png" /> 
+            <a aria-label="Chat on WhatsApp" href="https://wa.me/573003331333"><img alt="Chat on WhatsApp" src="../../public/what.png" /> 
             </a>
            </div>
            
@@ -40,7 +41,7 @@ export default function About(){
                   <h2 className = 'about_principal_titulo'>Descripción</h2>
                 </div>
                 <div class="card-backd">
-                  <p>El proyecto consiste en el desarrollo de una aplicación integral de gestión de accesos, diseñada para optimizar el control y registro de entradas y salidas de personas y objetos. Esta herramienta proporciona a los recepcionistas la capacidad de administrar de manera eficiente y segura el flujo de entrada y salida, así como realizar consultas detalladas sobre los registros almacenados. Además, ofrece la funcionalidad de exportar estos registros en formato Excel.</p>
+                  <p>El proyecto consiste en el desarrollo de una aplicación integral de gestión de accesos, diseñada para optimizar el control y registro de entradas y salidas de personas y objetos en un entorno empresarial. Esta herramienta proporciona a los recepcionistas la capacidad de administrar de manera eficiente y segura el flujo de entrada y salida, así como realizar consultas detalladas sobre los registros almacenados. Además, ofrece la funcionalidad de exportar estos registros en formato Excel.</p>
                 </div>
               </div>
             </div>
@@ -132,7 +133,7 @@ export default function About(){
                 <h3>Control de Usuarios</h3>
                 <div class="contenedor-card-item2" data-aos="zoom-in-up">
   		          <div class="contenedor-card-item-wrapper2">
-  		        <img src="./login.png" alt=""/>
+  		        <img src="../../public/login.png" alt=""/>
   		        <div class="contenedor-info2">
   		          <div class="info2">
   		            <p class="titulo2"></p>
@@ -147,7 +148,7 @@ export default function About(){
                 <h3>Gestión Entradas Y Salidas</h3>
                 <div class="contenedor-card-item2" data-aos="zoom-in-up">
   		          <div class="contenedor-card-item-wrapper2">
-  		        <img src="./es.png" alt=""/>
+  		        <img src="../../public/es.png" alt=""/>
   		        <div class="contenedor-info2">
   		          <div class="info2">
   		            <p class="titulo2"></p>
@@ -164,7 +165,7 @@ export default function About(){
                 <h3>Gestión de Usuarios</h3>
                 <div class="contenedor-card-item2" data-aos="zoom-in-up">
   		          <div class="contenedor-card-item-wrapper2">
-  		        <img src="./gobjetos.png" alt=""/>
+  		        <img src="../../public/gobjetos.png" alt=""/>
   		        <div class="contenedor-info2">
   		          <div class="info2">
   		            <p class="titulo2"></p>
@@ -179,7 +180,7 @@ export default function About(){
                 <h3>Gestión Objetos</h3>
                 <div class="contenedor-card-item2" data-aos="zoom-in-up">
   		          <div class="contenedor-card-item-wrapper2">
-  		        <img src="./gusuarios.png" alt=""/>
+  		        <img src="../../public/gusuarios.png" alt=""/>
   		        <div class="contenedor-info2">
   		          <div class="info2">
   		            <p class="titulo2"></p>
@@ -195,7 +196,7 @@ export default function About(){
                 <h3>Consultas y Exportación de Registros</h3>
                 <div class="contenedor-card-item2" data-aos="zoom-in-up">
   		          <div class="contenedor-card-item-wrapper2" >
-  		        <img src="./ges.png" alt=""/>
+  		        <img src="../../public/ges.png" alt=""/>
   		        <div class="contenedor-info2">
   		          <div class="info2">
   		            <p class="titulo2"></p>
@@ -209,8 +210,8 @@ export default function About(){
             </div>
           </div>
           <div className = 'seccioneslll' data-aos="zoom-out-up">
-            <div className = 'about_principal_funciones'>
-              <div class="parent">
+            <div className = 'about_principal_funciones' style={{flexDirection: 'row', justifyContent: 'center'}}>
+              <div class="parent" style={{margin: '10px'}}>
                 <div class="card">
                     <div class="logo">
                         <span class="circle circle1"></span>
@@ -228,8 +229,8 @@ export default function About(){
                     </div>
                     <div class="glass"></div>
                     <div class="content">
-                        <span class="title">Desarrollo</span>
-                        <span class="text">Se utilizaron tecnologías escalables y prácticas de programación para garantizar un rendimiento </span>
+                        <span class="title">Desarrollo y Ejecución</span>
+                        <span class="text">Pagina web dinamica con funcionalidades especificas</span>
                     </div>
                     <div class="bottom">
                         
@@ -258,6 +259,59 @@ export default function About(){
                         </div>
                         <div class="view-more">
                             <button class="view-more-button">Juan Florez Rodriguez</button>
+                            <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"></path></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+              <div class="parent" style={{margin: '10px'}}>
+                <div class="card">
+                    <div class="logo">
+                        <span class="circle circle1"></span>
+                        <span class="circle circle2"></span>
+                        <span class="circle circle3"></span>
+                        <span class="circle circle4"></span>
+                        <span class="circle circle5">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.667 31.69" class="svg">
+                                <path id="Path_6" data-name="Path 6" d="M12.827,1.628A1.561,1.561,0,0,1,14.31,0h2.964a1.561,1.561,0,0,1,1.483,1.628v11.9a9.252,9.252,0,0,1-2.432,6.852q-2.432,2.409-6.963,2.409T2.4,20.452Q0,18.094,0,13.669V1.628A1.561,1.561,0,0,1,1.483,0h2.98A1.561,1.561,0,0,1,5.947,1.628V13.191a5.635,5.635,0,0,0,.85,3.451,3.153,3.153,0,0,0,2.632,1.094,3.032,3.032,0,0,0,2.582-1.076,5.836,5.836,0,0,0,.816-3.486Z" transform="translate(0 0)"></path>
+                                <path id="Path_7" data-name="Path 7" d="M75.207,20.857a1.561,1.561,0,0,1-1.483,1.628h-2.98a1.561,1.561,0,0,1-1.483-1.628V1.628A1.561,1.561,0,0,1,70.743,0h2.98a1.561,1.561,0,0,1,1.483,1.628Z" transform="translate(-45.91 0)"></path>
+                                <path id="Path_8" data-name="Path 8" d="M0,80.018A1.561,1.561,0,0,1,1.483,78.39h26.7a1.561,1.561,0,0,1,1.483,1.628v2.006a1.561,1.561,0,0,1-1.483,1.628H1.483A1.561,1.561,0,0,1,0,82.025Z" transform="translate(0 -51.963)"></path>
+                              </svg>
+                        </span>
+        
+                    </div>
+                    <div class="glass"></div>
+                    <div class="content">
+                        <span class="title">Apoyo, Colaborador y Aplicacón</span>
+                        <span class="text">Gestión de requerimientos y stakeholder, aplicacion y apoyo en funcionalidad</span>
+                    </div>
+                    <div class="bottom">
+                        
+                        <div class="social-buttons-container">
+                            <button class="social-button .social-button1">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+</svg></button>
+                              <button class="social-button .social-button2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+</svg>
+                              </button>
+                              <button class="social-button .social-button3">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+  <path d="M8 11l0 5" />
+  <path d="M8 8l0 .01" />
+  <path d="M12 16l0 -5" />
+  <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+</svg>
+                              </button>
+                        </div>
+                        <div class="view-more">
+                            <button class="view-more-button">Juan Mass Guardo</button>
                             <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"></path></svg>
                         </div>
                     </div>
